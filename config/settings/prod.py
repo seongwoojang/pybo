982d152b0f3df720f8466aeae54da262dd3e564c
장성우 <jjjsw93@gmail.com>
2023-01-10 15:59:08 +0900

    프로젝트 게시판 준비

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['13.124.212.248']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'PASSWORD': 'Fhwlr93!',
+        'HOST': 'ls-ab4aa99fee1ed61217ace787743411ad809bff56.cy9bxtusp3br.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
